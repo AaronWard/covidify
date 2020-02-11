bc9fb43ccebe3f93a59caf9d100731abf51052dd
AaronWard <award40@LAMU02W63VHHTD6.uhc.com>
2020-02-11 13:58:57 +0000

    UPDATE - updated code to extract csv folders from new datasource (git repo by JHU)

--- src/data_prep.py
+++ src/data_prep.py
@@ -9,24 +9,21 @@ from datetime import datetime, date, time
 from dateutil.parser import parse
 from time import strftime
 import pyarrow
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 import json
+import git
 
-def clean_sheet_names(sheets):
+REPO = 'https://github.com/CSSEGISandData/2019-nCoV.git'
+TMP_FOLDER = '/tmp/corona/'
+TMP_GIT = os.path.join(TMP_FOLDER, '2019-nCoV')
+DATA = os.path.join(TMP_GIT, 'daily_case_updates')
+
+
+def clean_sheet_names(new_ranges):
     '''
     Get rid of the duplicate sheets, only take the sheets from the 
     latest point in the day
     '''
-    new_ranges = []
     indices = []
-    
-
-    #Get all the tabs in the sheet 
-    for s in sheets:
-        new_ranges.append(s.get("properties", {}).get("title"))
-        
     # Remove all sheets that dont have a numeric header
     new_ranges = [x for x in new_ranges if re.search(r'\d', x)]
         
@@ -43,107 +40,65 @@ def clean_sheet_names(sheets):
     # Return wanted tabs for the sheet extraction
     for index in sorted(indices):
         clean_new_ranges.append(new_ranges[index])
-
-    return clean_new_ranges
-    
-
-
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1wQVypefm946ch4XDp37uZ-wartW4V7ILdg-qYiDXUHM'
-
-"""Shows basic usage of the Sheets API.
-Prints values from a sample spreadsheet.
-"""
-
-creds = None
-# The file token.pickle stores the user's access and refresh tokens, and is
-# created automatically when the authorization flow completes for the first
-# time.
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
-# If there are no (valid) credentials available, let the user log in.
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            './src/credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open('./src/token.pickle', 'wb') as token:
-        pickle.dump(creds, token)
         
-#get all the sheet names for ranges when querying
-service = build('sheets', 'v4', credentials=creds)
-sheet_metadata = service.spreadsheets().get(spreadsheetId=SAMPLE_SPREADSHEET_ID).execute()
-sheets = sheet_metadata.get('sheets', '')
+    for sheet_name in clean_new_ranges:
+        print('...', sheet_name.split('_')[0])
+        
+    return clean_new_ranges
+
+def clone_repo(TMP_FOLDER, REPO):
+    print('Cloning Data Repo...')
+    git.Git(TMP_FOLDER).clone(REPO)
+
+# Create Tmp Folder
+if not os.path.isdir(TMP_FOLDER):
+    print('Creating folder...')
+    print('...', TMP_FOLDER)
+    os.mkdir(TMP_FOLDER)
+
+#Check if repo exists
+if not os.path.isdir(TMP_GIT):
+    clone_repo(TMP_FOLDER, REPO)
+else:
+    #get up to date repo
+    print('Deleting out of date repo...')
+    os.system('rm -rf ' + str(TMP_GIT))
+    clone_repo(TMP_FOLDER, REPO)
+    
+sheets = os.listdir(DATA)
 
 # Clean the result to the sheet tabs we want
-cleaned_ranges = clean_sheet_names(sheets)
+print('Cleaning sheets...')
+cleaned_sheets = clean_sheet_names(sheets)
+
 
 '''
 For assigning date by the time sheet name
 '''
-def fix_dates(tmp_df, tmp_sheet_range):
-    #Get year of entry
-    year = parse(tmp_df['Last Update'][0]).strftime("%Y")
-    
-    tmp_sheet_range = tmp_sheet_range.split('_')[0]
-    correct_date = datetime.strptime(tmp_sheet_range, '%b%d').strftime('%m/%d/' + year)    
-    tmp_df['Last Update'] = correct_date
-    
+def fix_dates(tmp_df, file_name):
+    file_name = file_name.split('_')[0]
+    tmp_df['Last Update'] = file_name
     return tmp_df
 
-# '''
-# For using the dates in the Last update cell
-# '''
-# def clean_dates(date):
-#     return parse(date).strftime("%m-%d-%Y %H:%M:%S").split(' ')[0]
+keep_cols = ['Confirmed', 'Country/Region', 'Deaths', 'Last Update', 'Province/State', 'Recovered']
+numeric_cols = ['Confirmed', 'Deaths', 'Recovered']
 
-def get_data(sheet_range):
-    tmp_df = pd.DataFrame([])
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=sheet_range).execute()
+def get_data(cleaned_sheets):
+    all_csv = []
+    # Import all CSV's
+    for file in sorted(cleaned_sheets):
+        tmp_df = pd.read_csv(os.path.join(DATA, file), index_col=None, header=0)
+        tmp_df = tmp_df[keep_cols]
+        tmp_df[numeric_cols] = tmp_df[numeric_cols].fillna(0)
+        tmp_df[numeric_cols] = tmp_df[numeric_cols].astype(int)
+        tmp_df = fix_dates(tmp_df, file)
+        all_csv.append(tmp_df)
 
-    header = result.get('values', [])[0]   # Assumes first line is header!
-    values = result.get('values', [])[1:]  # Everything else is data.
-    
-    
-    # rows with no deaths and recovered vals have shorter lists
-    # impute missing values with zeros
-    for i, row in enumerate(values):
-        if len(row) < len(header):
-            extra_zeros = (len(header) - len(row))
-            values[i] += [0] * extra_zeros
+    tmp_df = pd.concat(all_csv, axis=0, ignore_index=True, sort=True)
+    return tmp_df
 
-    # Create Dataframe
-    if not values:
-        print('No data found.')
-    else:
-        all_data = []
-        for col_id, col_name in enumerate(header):
-            column_data = []
-            for row in values:
-                column_data.append(row[col_id])
-            ds = pd.Series(data=column_data, name=col_name)
-            all_data.append(ds)
-        tmp = pd.concat(all_data, axis=1)
 
-        tmp = fix_dates(tmp, sheet_range)
-#         tmp['Last Update'] = tmp['Last Update'].apply(clean_dates)
-        
-    print('...', sheet_range)
-    return tmp
-   
-df_list = []
-print('Getting sheets to preprocess')
-for sheet_range in cleaned_ranges:
-    df_list.append(get_data(sheet_range))
+df = get_data(cleaned_sheets)
 
 # Now that we have all the data we now need to clean it 
 # - Fill null values
@@ -173,43 +128,26 @@ def clean_data(tmp_df):
     
     return tmp_df
 
-cleaned_dataframes = []
-
 print('Cleaning dataframes...')
-for frame in df_list:
-    cleaned_dataframes.append(clean_data(frame))
-
-
-#Impute the missing columns in the early stages with 0 values (recovered and deaths)
-print('Imputing missing columns...')
-cleaned_dataframes[-1]['recovered'] = [0] * (cleaned_dataframes[-1]).shape[0]
-cleaned_dataframes[-1]['deaths'] = [0] * (cleaned_dataframes[-1]).shape[0]
-
-cleaned_dataframes[-2]['recovered'] = [0] * (cleaned_dataframes[-2]).shape[0]
-cleaned_dataframes[-2]['deaths'] = [0] * (cleaned_dataframes[-2]).shape[0]
-
-print('Concatenating all sheet dataframes into one...')
-final_df = pd.concat(cleaned_dataframes, sort=True)
-
-# Make sure dates are all the same format
-final_df['date'] = final_df['date'].astype(str)
+df  = clean_data(df)
 
 # sheets need to be sorted by date value
 print('Sorting by date...')
-final_df = final_df.sort_values('date')
+df['date'] = df['date'].astype(str)
+df = df.sort_values('date')
 
-#Fix types
-for column in ['confirmed', 'deaths', 'recovered']:
-    final_df[column] = final_df[column].replace('', 0).astype(int)
 
 '''
 Get the difference of the sum totals for each
 date and plot them on a trendline graph
 '''
-def get_new_cases(final_df, col):
+def get_new_cases(tmp, col):
     diff_list = []
     tmp_df_list = []
-    df = final_df.copy()
+    df = tmp.copy()
+    
+    for column in ['confirmed', 'deaths', 'recovered']:
+        df[column] = df[column].replace('', 0).astype(int)
 
     for i, day in enumerate(df.date.unique()):    
         tmp_df = df[df.date == day]
@@ -224,10 +162,12 @@ def get_new_cases(final_df, col):
 
 print('Calculating dataframe for new cases...')
 daily_cases_df = pd.DataFrame([])
-daily_cases_df['new_confirmed_cases'] = get_new_cases(final_df, 'confirmed')
-daily_cases_df['new_deaths'] = get_new_cases(final_df, 'deaths')
-daily_cases_df['new_recoveries'] = get_new_cases(final_df, 'recovered')
-daily_cases_df['date'] = final_df.date.unique()
+daily_cases_df['new_confirmed_cases'] = get_new_cases(df, 'confirmed')
+daily_cases_df['new_deaths'] = get_new_cases(df, 'deaths')
+daily_cases_df['new_recoveries'] = get_new_cases(df, 'recovered')
+daily_cases_df['date'] = df.date.unique()
+
+
 
 '''
 Calculate the number of people that are ACTUALLY infected on a given day
@@ -235,11 +175,10 @@ currently infected = sum of people date - (recovored + died)
 ex: 5 = 10 - (4 - 1)
 
 '''
-
 current_infected = pd.DataFrame([])
-current_infected['currently_infected'] = (final_df.groupby('date').confirmed.sum() - \
-                                          (final_df.groupby('date').deaths.sum() + final_df.groupby('date').recovered.sum()))
-current_infected['delta'] = (current_infected['currently_infected'] - final_df.groupby('date').confirmed.sum())
+current_infected['currently_infected'] = (df.groupby('date').confirmed.sum() - \
+                                          (df.groupby('date').deaths.sum() + df.groupby('date').recovered.sum()))
+current_infected['delta'] = (current_infected['currently_infected'] - df.groupby('date').confirmed.sum())
 daily_cases_df = pd.merge(daily_cases_df, current_infected, how='outer', on='date')
 
 #Create date of extraction folder
@@ -253,12 +192,12 @@ if not os.path.exists(save_dir):
     
 print('Saving...')
 file_name = 'agg_data_{}.parquet.gzip'.format(datetime.date(datetime.now()))
-final_df.astype(str).to_parquet(os.path.join(save_dir, file_name), compression='gzip')
+df.astype(str).to_parquet(os.path.join(save_dir, file_name), compression='gzip')
 print('...', file_name)
 
 
 csv_file_name = 'agg_data_{}.csv'.format(datetime.date(datetime.now()))
-final_df.astype(str).to_csv(os.path.join(save_dir, csv_file_name))
+df.astype(str).to_csv(os.path.join(save_dir, csv_file_name))
 print('...', csv_file_name)
 
 
