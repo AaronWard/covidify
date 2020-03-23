e98184983414c171170f9bc8e4684d253425a688
AaronWard <award40@LAMU02W63VHHTD6.uhc.com>
2020-03-23 09:59:40 +0000

    UPDATE - cleaned up column definitions

--- src/covidify/config.py
+++ src/covidify/config.py
@@ -4,6 +4,7 @@ import os
 # CLI 
 #
 SCRIPT = '/pipeline.sh'
+LIST_SCRIPT = '/pipeline.sh'
 
 
 #
@@ -15,13 +16,15 @@ TMP_GIT = os.path.join(TMP_FOLDER, REPO.split('/')[-1].split('.')[0])
 DATA = os.path.join(TMP_GIT, 'csse_covid_19_data', 'csse_covid_19_daily_reports')
 
 #Github cols
-KEEP_COLS = ['Confirmed', 
-             'Country/Region', 
-             'Deaths', 
-             'Last Update', 
-             'Province/State', 
-             'Recovered']
+KEEP_COLS = ['country',
+             'province', 
+             'confirmed',
+             'deaths',
+             'recovered',
+             'date',
+             'datetime',
+             'file_date']
 
-NUMERIC_COLS = ['Confirmed', 
-                'Deaths', 
-                'Recovered']
+NUMERIC_COLS = ['confirmed', 
+                'deaths', 
+                'recovered']
