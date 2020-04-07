5ab5bbc865f153f85d1f74f152ea30b42ca860cd
AaronWard <award40@LAMU02W63VHHTD6.uhc.com>
2020-04-08 00:18:48 +0100

    ADD - added forecasting script into the pipeline

--- src/covidify/pipeline.sh
+++ src/covidify/pipeline.sh
@@ -11,6 +11,7 @@ OUT_FDR=$2
 SOURCE=$3
 COUNTRY=$4
 TOP_CNT=$5
+FRCST_DAYS=$6
 
 set -e
 
@@ -22,9 +23,14 @@ echo "... OUTPUT FOLDER: $OUT_FDR"
 echo "... DATA SOURCE: $SOURCE"
 echo "... COUNTRIES: $COUNTRY"
 echo "... TOP INFECTED COUNTRIES: $TOP_CNT"
+echo "... FORECAST PERIOD: $FRCST_DAYS"
+
 
 banner "Data Extraction"
 python $ENV/data_prep.py --output_folder $OUT_FDR --source $SOURCE --country $COUNTRY --top $TOP_CNT
+
+banner "Training Forecasting Model"
+python $ENV/forecast.py --output_folder $OUT_FDR --num_days $FRCST_DAYS
 
 banner "Data Visualization"
 python $ENV/data_visualization.py --output_folder $OUT_FDR --country $COUNTRY --top $TOP_CNT
