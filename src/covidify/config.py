d500eae84f1ea19ae59c1a91d10d4b799828125d
AaronWard <award40@LAMU02W63VHHTD6.uhc.com>
2020-04-08 00:21:47 +0100

    ADD - added parameters for forecasting to config

--- src/covidify/config.py
+++ src/covidify/config.py
@@ -16,6 +16,19 @@ TMP_GIT = os.path.join(TMP_FOLDER, REPO.split('/')[-1].split('.')[0])
 DATA = os.path.join(TMP_GIT, 'csse_covid_19_data', 'csse_covid_19_daily_reports')
 LOG_TOP_N_COUNTRIES = 10
 
+
+#
+# FORECASTING
+#
+DAYS_IN_FUTURE = 10 # Amount of days you want to forecast into future
+PERC_SPLIT = 0.95   # Train / test split for forecasting model.
+
+#
+# DATA VISUALIZATION
+#
+FIG_SIZE = (14,10)
+
+
 #Github cols
 KEEP_COLS = ['country',
              'province', 
