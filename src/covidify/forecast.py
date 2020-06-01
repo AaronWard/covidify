c3f522505cc99938ad81932b6e399a58ab3ec9c4
AaronWard <aaronward6210@gmail.com>
2020-06-01 17:10:15 +0100

    UPDATE - updated forecasting script to use all data instead of splitting into train/test

--- src/covidify/forecast.py
+++ src/covidify/forecast.py
@@ -41,18 +41,27 @@ days_in_future = int(args['--num_days'])
 # file paths
 image_dir =  os.path.join(out,'reports', 'images')
 trend_file  = 'trend_{}.csv'.format(datetime.date(datetime.now()))
-forecast_file  = 'forecast_{}.csv'.format(datetime.date(datetime.now()))
+forecast_file  = 'forecast_{}.csv'.format(datetime.date(datetime.now())) # For saving forecasts
 data_dir  = os.path.join(out, 'data', str(datetime.date(datetime.now())))
+trend_df = pd.read_csv(os.path.join(data_dir, trend_file)).reset_index(drop=True)
 
 
-trend_df = pd.read_csv(os.path.join(data_dir, trend_file))
+# For forecasting (use all data for training)
+train_start = datetime.strptime(trend_df.date.min(), "%Y-%m-%d")
+train_end = datetime.strptime(trend_df.date.max(), "%Y-%m-%d")
+forecast_start = datetime.strptime(trend_df.date.max(), "%Y-%m-%d") + timedelta(days=1)
+forecast_end = datetime.strptime(trend_df.date.max(), "%Y-%m-%d") + timedelta(days=days_in_future+1) # Extra day because of one day lag
+
+train_period = [d.strftime('%Y-%m-%d') for d in pd.date_range(train_start, train_end)]
+forecast_period = [d.strftime('%Y-%m-%d') for d in pd.date_range(forecast_start, forecast_end)]
+
 
 if not os.path.exists(image_dir):
     print('Creating reports folder...')
     os.system('mkdir -p ' + image_dir)
 
 
-def plot_forecast(tmp_df, train, valid, index_forecast, forecast, confint):
+def plot_forecast(tmp_df, train, index_forecast, forecast, confint):
     '''
     Plot the values of train and test, the predictions from ARIMA and the shadowing
     for the confidence interval.
@@ -66,9 +75,8 @@ def plot_forecast(tmp_df, train, valid, index_forecast, forecast, confint):
     print('... saving graph')
     fig, ax = plt.subplots(figsize=FIG_SIZE)
     plt.title('ARIMA - Prediction for cumalitive case counts {} days in the future'.format(days_in_future))    
-    plt.plot(train, label='Train',marker='o')
-    plt.plot(valid, label='Test', marker='o')
-    plt.plot(forecast, label='Forecast', marker='o')
+    plt.plot(tmp_df.cumulative_cases, label='Train',marker='o')
+    plt.plot(tmp_df.pred, label='Forecast', marker='o')
     tmp_df.groupby('date')[['']].sum().plot(ax=ax)
     plt.fill_between(index_forecast, 
                      upper_series, 
@@ -81,47 +89,28 @@ def plot_forecast(tmp_df, train, valid, index_forecast, forecast, confint):
     fig.savefig(os.path.join(image_dir, 'cumulative_forecasts.png'))
 
 
-def forecast(tmp_df, t, v, days_in_future):
-    train = t.cumulative_cases
-    valid = v.cumulative_cases
-    index_forecast = [x for x in range(valid.index[0],valid.index[-1] + days_in_future+1)]
+def forecast(tmp_df, train, index_forecast, days_in_future):
     
     # Fit model with training data
     model = auto_arima(train, trace=False, error_action='ignore', suppress_warnings=True)
     model_fit = model.fit(train)
         
     forecast, confint = model_fit.predict(n_periods=len(index_forecast), return_conf_int=True)
-    RMSE = sqrt(mean_squared_error(valid,forecast[:len(valid)]))
-    print('... RMSE:', RMSE)
-    print('... forecasting {} days in the future'.format(days_in_future))
 
-        
-    #For plotting date ranges
-    date_1 = datetime.strptime(tmp_df.date.min(), "%Y-%m-%d")
-    date_2 = datetime.strptime(tmp_df.date.max(), "%Y-%m-%d") + timedelta(days=days_in_future)
-    date_range = [d.strftime('%Y-%m-%d') for d in pd.date_range(date_1, date_2)]
-    date_range = pd.date_range(date_1, date_2)
-    
-    forecast = pd.DataFrame(forecast, index = index_forecast, columns=['pred'])
-    tmp_df = pd.concat([tmp_df, forecast], axis=1, sort=False)
-    tmp_df['date'] = pd.Series(date_range).astype(str)
-    tmp_df[''] = None #Dates get messed up, so need to use pandas plotting
+    forecast_df = pd.concat([tmp_df, pd.DataFrame(forecast, index = index_forecast, columns=['pred'])], axis=1, sort=False)
+    date_range = [d.strftime('%Y-%m-%d') for d in pd.date_range(train_start, forecast_end)]
+    forecast_df['date'] = pd.Series(date_range).astype(str)
+    forecast_df[''] = None # Dates get messed up, so need to use pandas plotting
         
     # Save Model and file
     print('... saving file:', forecast_file)
-    tmp_df.to_csv(os.path.join(data_dir, forecast_file))
+    forecast_df.to_csv(os.path.join(data_dir, forecast_file))
         
-    plot_forecast(tmp_df, train, valid, index_forecast, forecast, confint)
+    plot_forecast(forecast_df, train, index_forecast, forecast, confint)
     
-    
-def split_data(tmp_df):
-    print('... train/test split:', PERC_SPLIT)
-    #Split data
-    train = tmp_df[:int(PERC_SPLIT*(len(tmp_df)))]
-    valid = tmp_df[int(PERC_SPLIT*(len(tmp_df))):]
-
-    forecast(tmp_df, train, valid, days_in_future)
-
 if __name__ == '__main__':
     print('Training forecasting model...')
-    split_data(trend_df)
+
+    train = trend_df[trend_df.date.isin(train_period)].cumulative_cases
+    index_forecast = [x for x in range(train.index[-1]+1, train.index[-1] + days_in_future+1)]
+    forecast(trend_df, train, index_forecast, days_in_future)
