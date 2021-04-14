002ffdf74f034b2b51b99cf6ca89df2d6125af23
AttSan <attsan1908@gmail.com>
2021-04-13 22:03:38 -0400

    Applied a Builder Creational Design Pattern to the data_prep, data_visualization & forecast modules. Ensures a clear separation between construction of specific products requested from their basic representations (most simplified form).

--- src/covidify/data_prep.py
+++ src/covidify/data_prep.py
@@ -24,16 +24,13 @@ from datetime import datetime, date, time
 from covidify.sources import github, wiki
 from covidify.config import REPO, TMP_FOLDER, TMP_GIT, DATA
 from covidify.utils.utils import replace_arg_score
-
+from abc import ABCMeta, abstractmethod
 
 args = docopt.docopt(__doc__)
 out = args['--output_folder']
 country = args['--country']
 source = args['--source']
 top = int(args['--top'])
-
-
-############ DATA SELECTION ############
 
 if '_' in country:
     country = replace_arg_score(country)
@@ -48,111 +45,28 @@ elif source == 'wiki':
     print('Apologies, the wikipedia source is not ready yet - getting github data')
     df = github.get()
     
-
-
-############ COUNTRY SELECTION ############
-
-def get_similar_countries(c, country_list):
-    pos_countries = get_close_matches(c, country_list)
-    
-    if len(pos_countries) > 0:
-        print('\033[1;31m'+c, 'was not listed. did you mean', pos_countries[0].capitalize() + '?\033[0;0m')
-        
-        #Only delete if its a covidify generated folder
-        if 'Desktop/covidify-output-' in out:
-            os.system('rm -rf ' + out)
-        sys.exit(1)
-    else:
-        print('\033[1;31m'+c, 'was not listed.\033[0;0m')
-        if 'Desktop/covidify-output-' in out:
-            os.system('rm -rf ' + out)
-        sys.exit(1)
-        
-def check_specified_country(df, country):
-    '''
-    let user filter reports by country, if not found
-    then give a option if the string is similar
-    '''
-    
-    # Get all unique countries in the data
-    country_list = list(map(lambda x:x.lower().strip(), set(df.country.values)))
-
-    if country:
-        print('Country specified!')
-        if country.lower() == 'Mainland China': #Mainland china and china doesn't come up as similar
-            print(country, 'was not listed. did you mean China?')
-            sys.exit(1)
-        # give similar option if similarity found
-        if country.lower() not in country_list:
-            get_similar_countries(country, country_list)
-            
-        else:
-            #Return filtered dataframe
-            print('... filtering data for', country)
-            if len(country) == 2:
-                df = df[df.country == country.upper()]
-            else:
-                df = df[df.country == capwords(country)]
-            return df
-    else:
-        print('... No specific country specified')
-        return df
-
-df = check_specified_country(df, country)
+PRODUCT = Director.construct()
 
 ############ DAILY CASES ############
-
-# sheets need to be sorted by date value
-# print('Sorting by datetime...')
+# sheets need to be sorted by date value - print('Sorting by datetime...')
 df = df.sort_values('datetime')
 
 current_date = str(datetime.date(datetime.now()))
-
-'''
-Get the difference of the sum totals for each
-date and plot them on a trendline graph
-'''
-def get_new_cases(tmp, col):
-    diff_list = []
-    tmp_df_list = []
-    df = tmp.copy()
-
-    for i, day in enumerate(df.sort_values('file_date').file_date.unique()):
-        tmp_df = df[df.file_date == day]
-        tmp_df_list.append(tmp_df[col].sum())
-
-        if i == 0:
-            diff_list.append(tmp_df[col].sum())
-        else:
-            diff_list.append(tmp_df[col].sum() - tmp_df_list[i-1])
-
-    return diff_list
-
-def get_moving_average(tmp, col):
-    df = tmp.copy()
-    return df[col].rolling(window=2).mean()
-
-def get_exp_moving_average(tmp, col):
-    df = tmp.copy()
-    return df[col].ewm(span=2, adjust=True).mean()
-
 
 print('... Calculating dataframe for new cases')
 daily_cases_df = pd.DataFrame([])
 daily_cases_df['date'] = df.file_date.unique()
 daily_cases_df = daily_cases_df.sort_values('date')
-daily_cases_df['new_confirmed_cases'] = get_new_cases(df, 'confirmed')
-daily_cases_df['new_deaths'] = get_new_cases(df, 'deaths')
-daily_cases_df['new_recoveries'] = get_new_cases(df, 'recovered')
+daily_cases_df['new_confirmed_cases'] = Builder.get_new_cases(df, 'confirmed')
+daily_cases_df['new_deaths'] = Builder.get_new_cases(df, 'deaths')
+daily_cases_df['new_recoveries'] = Builder.get_new_cases(df, 'recovered')
 daily_cases_df['cumulative_cases'] = daily_cases_df.new_confirmed_cases.cumsum()
 daily_cases_df.insert(loc=0, column='day', value=np.arange(0, len(daily_cases_df)))
 
-'''
-Calculate the number of people that are ACTUALLY infected on a given day
-currently infected = sum of people date - (recovored + died)
+"""
+Calculate the number of people that are ACTUALLY infected on a given day -> currently infected = sum of people date - (recovered + died)
 ex: 5 = 10 - (4 - 1)
-
-'''
+"""
 current_infected = pd.DataFrame([])
 current_infected['currently_infected'] = (df.groupby('file_date').confirmed.sum() - (df.groupby('file_date').deaths.sum() + df.groupby('file_date').recovered.sum()))
 current_infected['delta'] = (current_infected['currently_infected'] - df.groupby('file_date').confirmed.sum())
@@ -165,46 +79,18 @@ daily_cases_df = pd.merge(daily_cases_df, current_infected, how='outer', on='dat
 print('Calculating data for logarithmic plotting...')
 if not country:
     print('... top infected countries: {}'.format(top))
-
-def get_top_countries(data):
-    # Get top N infected countries
-    tmp_df = data.copy()
-    tmp_df = tmp_df[tmp_df.file_date == df.file_date.max()]
-    return tmp_df.groupby(['country']).agg({'confirmed': 'sum'}).sort_values('confirmed',ascending=False).head(top).index 
         
-TOP_N_COUNTRIES = get_top_countries(df)    
-
+TOP_N_COUNTRIES = Builder.get_top_countries(df)    
 tmp_df = df[df.country.isin(TOP_N_COUNTRIES)].copy()
-
-def get_day_counts(d, country):
-    '''
-    For each country, get the days of the spread since 500
-    cases
-    '''
-    data = d.copy()
-    result_df = pd.DataFrame([])
-    result_df = data.groupby(['file_date']).agg({'confirmed': 'sum',
-                                                'recovered': 'sum',
-                                                'deaths': 'sum'})
-    result_df['date'] = data['file_date'].unique()
-    result_df['country'] = country
-        
-    result_df = result_df[result_df.confirmed >= 500]
-    result_df.insert(loc=0, column='day', value=np.arange(len(result_df)))
-    return result_df
-
 df_list = []
 
 for country in TOP_N_COUNTRIES:
-    print('   ...', country + ': ' +  str(tmp_df[(tmp_df.file_date == df.file_date.max()) & 
-                                                 (tmp_df.country == country)].confirmed.sum()))
-    df_list.append(get_day_counts(tmp_df[tmp_df.country == country], country))
+    print('   ...', country + ': ' +  str(tmp_df[(tmp_df.file_date == df.file_date.max()) &  (tmp_df.country == country)].confirmed.sum()))
+    df_list.append(Builder.get_day_counts(tmp_df[tmp_df.country == country], country))
     
 log_df = pd.concat(df_list, axis=0, ignore_index=True)
 
-
-############ SAVE DATA ############
-#Create date of extraction folder
+#Create and save date of extraction folder
 data_folder = os.path.join('data', str(datetime.date(datetime.now())))
 save_dir = os.path.join(out, data_folder)
 
@@ -228,3 +114,136 @@ log_df.astype(str).to_csv(os.path.join(save_dir, log_file_name))
 print('...', log_file_name)
 
 print('Done!')
+
+class IBuilder(metaclass=ABCMeta):
+    def get_similar_countries(self, c, country_list):
+        "get all countries in list"
+
+    def check_specified_country(self, df, country):
+        "let user filter reports by country, if not found then give a option if the string is similar"
+        "Get all unique countries in the data"
+
+    def get_new_cases(self, tmp, col):
+        ""
+
+    def get_moving_average(self, tmp, col):
+        ""
+
+    def get_exp_moving_average(self, tmp, col):
+        ""
+    
+    def get_top_countries(self, data):
+        "Get top N infected countries"
+
+    def get_day_counts(self, d, country):
+        "For each country, get the days of the spread since 500 cases"
+
+class Builder(IBuilder):
+    def __init__(self):
+        self.product = Product()
+
+    def get_similar_countries(self, c, country_list):
+        pos_countries = get_close_matches(c, country_list)
+        self.c = c
+        self.country_list = country_list
+        if len(pos_countries) > 0:
+            print('\033[1;31m'+c, 'was not listed. did you mean', pos_countries[0].capitalize() + '?\033[0;0m')
+            
+            #Only delete if its a covidify generated folder
+            if 'Desktop/covidify-output-' in out:
+                os.system('rm -rf ' + out)
+            sys.exit(1)
+        else:
+            print('\033[1;31m'+c, 'was not listed.\033[0;0m')
+            if 'Desktop/covidify-output-' in out:
+                os.system('rm -rf ' + out)
+            sys.exit(1)
+            
+    def check_specified_country(self, df, country):
+        # let user filter reports by country, if not found give a option if the string is similar & Get all unique countries in the data
+        self.country_list = list(map(lambda x:x.lower().strip(), set(df.country.values)))
+        self.df = check_specified_country(df, country)
+        self.country = country
+        if country:
+            print('Country specified!')
+            if country.lower() == 'Mainland China': #Mainland china and china doesn't come up as similar
+                print(country, 'was not listed. did you mean China?')
+                sys.exit(1)
+            # give similar option if similarity found
+            if country.lower() not in country_list:
+                get_similar_countries(country, country_list)
+                
+            else:
+                #Return filtered dataframe
+                print('... filtering data for', country)
+                if len(country) == 2:
+                    df = df[df.country == country.upper()]
+                else:
+                    df = df[df.country == capwords(country)]
+                return df
+        else:
+            print('... No specific country specified')
+            df = check_specified_country(df, country)
+            return df
+
+    # Get the difference of the sum totals for each date and plot them on a trendline graph
+    def get_new_cases(self, tmp, col):
+        diff_list = []
+        tmp_df_list = []
+        df = tmp.copy()
+
+        for i, day in enumerate(df.sort_values('file_date').file_date.uznique()):
+            tmp_df = df[df.file_date == day]
+            tmp_df_list.append(tmp_df[col].sum())
+
+            if i == 0:
+                diff_list.append(tmp_df[col].sum())
+            else:
+                diff_list.append(tmp_df[col].sum() - tmp_df_list[i-1])
+
+        return diff_list
+
+    def get_moving_average(self, tmp, col):
+        df = tmp.copy()
+        return df[col].rolling(window=2).mean()
+
+    def get_exp_moving_average(self, tmp, col):
+        df = tmp.copy()
+        return df[col].ewm(span=2, adjust=True).mean()
+
+    def get_top_countries(self, data):
+        # Get top N infected countries
+        tmp_df = data.copy()
+        tmp_df = tmp_df[tmp_df.file_date == df.file_date.max()]
+        self.data = data
+        return tmp_df.groupby(['country']).agg({'confirmed': 'sum'}).sort_values('confirmed',ascending=False).head(top).index 
+
+    def get_day_counts(self, d, country):
+        # For each country, get the days of the spread since 500 cases
+        data = d.copy()
+        result_df = pd.DataFrame([])
+        result_df = data.groupby(['file_date']).agg({'confirmed': 'sum', 'recovered': 'sum', 'deaths': 'sum'})
+        result_df['date'] = data['file_date'].unique()
+        result_df['country'] = country
+            
+        result_df = result_df[result_df.confirmed >= 500]
+        result_df.insert(loc=0, column='day', value=np.arange(len(result_df)))
+        return result_df
+
+class Product():
+    def __init__(self):
+        self.c = ""
+        self.country_list = []
+        self.df = pd.DataFrame(data=None, index=None, columns=None, dtype=None, copy=None)
+        self.data = pd.DataFrame(data=None, index=None, columns=None, dtype=None, copy=None)
+        self.country = self.fig_title = self.col = self.graph_type = ""
+        self.df_list = []
+
+class Director():
+    @staticmethod
+    def construct():
+        return Builder()\
+        .check_specified_country(df, country)\
+        .get_new_cases(df, 'recovered')\
+        .get_new_cases(df, 'deaths')\
+        .get_new_cases(df, 'confirmed')
--- src/covidify/data_visualization.py
+++ src/covidify/data_visualization.py
@@ -22,6 +22,7 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 from covidify.utils.utils import replace_arg_score
+from abc import ABCMeta, abstractmethod
 
 # plt settings
 font = {'weight' : 'bold',
@@ -34,27 +35,20 @@ out = args['--output_folder']
 country = args['--country']
 top = int(args['--top'])
 
-
 if '_' in country:
     country = replace_arg_score(country)
 
 if country == 'Global':
     country = None
 
-#change report name if country specified
-def create_report_name(country):
-    if country:
-        return '{}_report_{}.xlsx'.format(country, datetime.date(datetime.now()))
-    else:
-        return 'report_{}.xlsx'.format(datetime.date(datetime.now()))
+PRODUCT = Director.construct()
 
 # Dynamic parameters
 data_dir  = os.path.join(out, 'data', str(datetime.date(datetime.now())))
 agg_file  = 'agg_data_{}.csv'.format(datetime.date(datetime.now()))
 trend_file  = 'trend_{}.csv'.format(datetime.date(datetime.now()))
 log_file  = 'log_{}.csv'.format(datetime.date(datetime.now()))
-report  = create_report_name(country)
-
+report  = Builder.create_report_name(country)
 
 # import data
 print('Importing Data...')
@@ -62,7 +56,7 @@ agg_df = pd.read_csv(os.path.join(data_dir, agg_file))
 daily_df = pd.read_csv(os.path.join(data_dir, trend_file))
 log_df = pd.read_csv(os.path.join(data_dir, log_file))
 
-#Create place to save diagrams
+# Create place to save diagrams
 image_dir =  os.path.join(out,'reports', 'images')
 reports_dir =  os.path.join(out,'reports')
 
@@ -72,87 +66,23 @@ if not os.path.exists(image_dir):
 
 # Convert types
 for col in ['confirmed', 'deaths', 'recovered']:
-    agg_df[col] = agg_df[col].replace('', 0).astype(int)
+    agg_df[col] = agg_df[col].replace('', 0).astype(int)   
 
-
-##### Define Graphs #####
-#Change titles and saved file names if country
-#is specified
-def create_title(fig_title, country):
-    if country:
-        return fig_title + ' for ' + country
-    else:
-        return fig_title
-    
-def create_save_file(col, country, graph_type):
-    if country:
-        return '{}_{}_{}.png'.format(country, col, graph_type)
-    else:
-        return '{}_{}.png'.format(col, graph_type)
-
-# Plot and save trendline graph
-def create_trend_line(tmp_df, date_col, col, col2, col3, fig_title, country):
-    fig, ax = plt.subplots(figsize=(20,10))
-    tmp_df.groupby([date_col])[[col, col2, col3]].sum().plot(ax=ax, marker='o')
-    ax.set_title(create_title(fig_title, country))
-    fig = ax.get_figure()
-    fig.savefig(os.path.join(image_dir, create_save_file(col, country, 'trendline')))
-
-def create_bar(tmp_df, col, rgb, country):
-    tmp_df = tmp_df.tail(120)
-    fig, ax = plt.subplots(figsize=(20,10))
-    tmp = tmp_df.groupby(['date'])[[col]].sum()
-    ax.set_title(create_title(col, country))
-    tmp.plot.bar(ax=ax, rot=90, color=rgb)
-    fig = ax.get_figure()
-    fig.savefig(os.path.join(image_dir, create_save_file(col, country, 'bar')))
-
-def create_stacked_bar(tmp_df, col1, col2, fig_title, country):
-    tmp_df = tmp_df.tail(120)
-    tmp_df = tmp_df.set_index('date')
-    fig, ax = plt.subplots(figsize=(20,10))
-    ax.set_title(create_title(fig_title, country))
-    tmp_df[[col2, col1]].plot.bar(ax=ax,
-                                  rot=90,
-                                  stacked=True)
-    fig = ax.get_figure()
-    fig.savefig(os.path.join(image_dir, create_save_file(col2, country, 'stacked_bar')))
-
-def log_plot(tmp, col, fig_title):
-    '''
-    Plot on a logarithmic scale for comparing
-    countries infection rates
-    
-    '''
-    cm = plt.get_cmap('gist_rainbow')
-    fig = plt.figure(figsize = (20,10))
-    ax = fig.add_subplot(111)
-    ax.set_prop_cycle('color', [cm(1.*i/top) for i in range(top)])
-    for country in tmp.country.unique():
-        new_col = country.lower() + '_' + col
-        tmp[country.lower() + '_' + col] = tmp[[col]]
-        tmp.rename(columns={col: new_col})
-        tmp[tmp.country == country].groupby(['day'])[[new_col]].sum().plot(ax=ax)
-        ax.set_yscale('log', basey=10)
-    ax.set_title(fig_title)
-    fig = ax.get_figure()
-    fig.savefig(os.path.join(image_dir, create_save_file(col, country=None, graph_type='log')))
-    
 ##### Create Graphs #####
 print('Creating graphs...')
 print('... Time Series Trend Line')
 # Time Series Data Plots
-create_trend_line(agg_df, 'file_date', 'confirmed', 'deaths', 'recovered', 'Accumulative trend', country)
+Builder.create_trend_line(agg_df, 'file_date', 'confirmed', 'deaths', 'recovered', 'Accumulative trend', country)
 
 
 print('... Daily Figures')
 # Daily Figures Data Plots
 daily_figures_cols = ['new_confirmed_cases', 'new_deaths', 'new_recoveries', 'currently_infected']
 for col, rgb in zip(daily_figures_cols, ['tomato', 'lightblue', 'mediumpurple', 'green']):
-    create_bar(daily_df, col, rgb, country)
+    Builder.create_bar(daily_df, col, rgb, country)
 
 # Trend line for new cases
-create_trend_line(daily_df, 'date', 'new_confirmed_cases', 'new_deaths', 'new_recoveries', 'Daily trendline', country)
+Builder.create_trend_line(daily_df, 'date', 'new_confirmed_cases', 'new_deaths', 'new_recoveries', 'Daily trendline', country)
 
 
 print('... Daily New Infections Differences')
@@ -160,11 +90,11 @@ new_df = pd.DataFrame([])
 new_df['date'] = daily_df['date']
 new_df['confirmed_cases'] = agg_df.groupby(['file_date']).confirmed.sum().values - daily_df.new_confirmed_cases
 new_df['new_confirmed_cases'] = daily_df.new_confirmed_cases
-create_stacked_bar(new_df, 'new_confirmed_cases', 'confirmed_cases', "Stacked bar of confirmed and new cases by day", country)
+#Builder.create_stacked_bar(new_df, 'new_confirmed_cases', 'confirmed_cases', "Stacked bar of confirmed and new cases by day", country)
 
 
 print('... Logarithmic plots')
-log_plot(log_df, 'confirmed', 'Logarithmic plots for top {} most infected countries\nStarting from days since first 500 confirmed cases.'.format(top))
+#Builder.log_plot(log_df, 'confirmed', 'Logarithmic plots for top {} most infected countries\nStarting from days since first 500 confirmed cases.'.format(top))
 
 ### Create Excel Spreadsheet ###
 print('Creating excel spreadsheet report...')
@@ -174,31 +104,12 @@ workbook_writer = pd.ExcelWriter(os.path.join(reports_dir, report), engine='xlsx
 daily_df.to_excel(workbook_writer, sheet_name='daily figures')  
 workbook = workbook_writer.book
 
-def get_image_types(path):
-    '''
-    get all the possible types of images in
-    the passed directory path
-    '''
-    types = []
-    for fn in glob.glob(os.path.join(path, '*.png')):
-        types.append(fn.split('_',)[-1].split('.')[0])
-    
-    return types
-
-# Get all images for each type
-def read_images(path, graph_type):
-    image_list = []
-    for fn in glob.glob(os.path.join(path, '*_{}.png'.format(graph_type))):
-        image_list.append(fn)    
-    images = {graph_type : image_list}
-    return dict(images)
-
-image_types = get_image_types(image_dir)
+#image_types = Builder.get_image_types(image_dir)
 
 padding = 1 # Set padding for images in spreadsheet
-for types in set(image_types):
+for types in set(Builder.get_image_types(image_dir)): # set(image_types)
     print('... reading images for:', types)
-    type_dict = read_images(image_dir, types)
+    type_dict = Builder.read_images(image_dir, types)
     
     # Add image to the worksheet
     worksheet = workbook.add_worksheet(name='{}_graphs'.format(types))
@@ -209,3 +120,146 @@ for types in set(image_types):
     
 workbook.close()
 print('Done!')
+
+class IBuilder(metaclass=ABCMeta):
+    @staticmethod
+    def create_report_name(self, country):
+        "Change report name if country specified, otherwise default"
+
+    @staticmethod
+    def create_title(fig_title, country):
+        "Change title to new country, otherwise create default title"
+
+    @staticmethod
+    def create_save_file(col, country, graph_type):
+        "Change file name to new country, otherwise create default file name"
+
+    @staticmethod
+    def create_trend_line(tmp_df, date_col, col, col2, col3, fig_title, country):
+        "Plot and save trendline graph"
+
+    @staticmethod
+    def create_bar(tmp_df, col, rgb, country):
+        "Plot and save bar graph"
+
+    @staticmethod
+    def create_stacked_bar(tmp_df, col1, col2, fig_title, country):
+        "Plot and save stacked graph"
+
+    @staticmethod
+    def log_plot(tmp, col, fig_title):
+        "Plot logarithmic scale to compare countries infection rates"
+
+    @staticmethod
+    def get_image_types(path):
+        "Get types for all images"
+
+    @staticmethod
+    def read_images(path, graph_type):
+        "Get all images for each type"
+
+class Builder(IBuilder):
+    def __init__(self):
+        self.product = Product()
+
+    #change report name if country specified
+    def create_report_name(self, country):
+        if country:
+            self.report = '{}_report_{}.xlsx'.format(country, datetime.date(datetime.now()))
+            self.country = country
+            # return '{}_report_{}.xlsx'.format(country, datetime.date(datetime.now()))
+            return self
+        else:
+            return self #'report_{}.xlsx'.format(datetime.date(datetime.now()))
+
+    def create_title(self, fig_title, country):
+        if country:
+            self.fig_title = fig_title
+            self.country = country
+            return fig_title + ' for ' + country
+        else:
+            return fig_title 
+    
+    def create_save_file(self, col, country, graph_type):
+        if country:
+            self.col = col
+            self.country = country
+            self.graph_type = graph_type
+            return '{}_{}_{}.png'.format(country, col, graph_type)
+        else:
+            return '{}_{}.png'.format(col, graph_type)
+
+    # Plot and save trendline graph
+    def create_trend_line(self, tmp_df, date_col, col, col2, col3, fig_title, country):
+        fig, ax = plt.subplots(figsize=(20,10))
+        tmp_df.groupby([date_col])[[col, col2, col3]].sum().plot(ax=ax, marker='o')
+        ax.set_title(create_title(fig_title, country))
+        fig = ax.get_figure()
+        fig.savefig(os.path.join(image_dir, create_save_file(col, country, 'trendline')))
+
+    def create_bar(self, tmp_df, col, rgb, country):
+        tmp_df = tmp_df.tail(120)
+        fig, ax = plt.subplots(figsize=(20,10))
+        tmp = tmp_df.groupby(['date'])[[col]].sum()
+        ax.set_title(create_title(col, country))
+        tmp.plot.bar(ax=ax, rot=90, color=rgb)
+        fig = ax.get_figure()
+        fig.savefig(os.path.join(image_dir, create_save_file(col, country, 'bar')))
+
+    def create_stacked_bar(self, tmp_df, col1, col2, fig_title, country):
+        tmp_df = tmp_df.tail(120)
+        tmp_df = tmp_df.set_index('date')
+        fig, ax = plt.subplots(figsize=(20,10))
+        ax.set_title(create_title(fig_title, country))
+        tmp_df[[col2, col1]].plot.bar(ax=ax, rot=90, stacked=True)
+        fig = ax.get_figure()
+        fig.savefig(os.path.join(image_dir, create_save_file(col2, country, 'stacked_bar')))
+
+    def log_plot(self, tmp, col, fig_title):
+        #Plot on a logarithmic scale for comparing countries infection rates
+        cm = plt.get_cmap('gist_rainbow')
+        fig = plt.figure(figsize = (20,10))
+        ax = fig.add_subplot(111)
+        ax.set_prop_cycle('color', [cm(1.*i/top) for i in range(top)])
+        for country in tmp.country.unique():
+            new_col = country.lower() + '_' + col
+            tmp[country.lower() + '_' + col] = tmp[[col]]
+            tmp.rename(columns={col: new_col})
+            tmp[tmp.country == country].groupby(['day'])[[new_col]].sum().plot(ax=ax)
+            ax.set_yscale('log', basey=10)
+        ax.set_title(fig_title)
+        fig = ax.get_figure()
+        fig.savefig(os.path.join(image_dir, create_save_file(col, country=None, graph_type='log')))
+
+    def get_image_types(self, path):
+        # get all the possible types of images in the passed directory path
+        types = []
+        for fn in glob.glob(os.path.join(path, '*.png')):
+            types.append(fn.split('_',)[-1].split('.')[0])
+        return types
+
+    # Get all images for each type
+    def read_images(self, path, graph_type):
+        image_list = []
+        for fn in glob.glob(os.path.join(path, '*_{}.png'.format(graph_type))):
+            image_list.append(fn)    
+        images = {graph_type : image_list}
+        return dict(images)
+
+class Product():
+    def __init__(self):
+        "initialize as empty"
+        self.tmp = pd.DataFrame(data=None, index=None, columns=None, dtype=None, copy=None)
+        self.tmp_df = pd.DataFrame(data=None, index=None, columns=None, dtype=None, copy=None)
+        self.report = 'report_{}.xlsx'.format(datetime.date(datetime.now()))
+        self.country = self.fig_title = self.col = self.graph_type = ""
+
+class Director():
+    @staticmethod
+    def construct():
+        return Builder()\
+        .create_report_name(country)\
+        .create_title(Product.fig_title, country)\
+        .create_save_file(col, country, 'log')\
+        .create_stacked_bar(new_df, 'new_confirmed_cases', 'confirmed_cases', "Stacked bar of confirmed and new cases by day", country)\
+        .log_plot(log_df, 'confirmed', 'Logarithmic plots for top {} most infected countries\nStarting from days since first 500 confirmed cases.'.format(top))
