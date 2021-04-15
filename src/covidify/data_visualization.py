2b373b0e615163abe10ef24944c9dc71743af9c1
Saleem Qureshi <77658916+saleemq1@users.noreply.github.com>
2021-04-15 18:13:55 -0400

    Add Structural Pattern
    Structural Pattern

--- src/covidify/data_visualization.py
+++ src/covidify/data_visualization.py
@@ -1,211 +1,106 @@
-"""
-data_visualization.py - Extract data from date range and create models
-Usage:
-    data_visualization.py [options]
-    data_visualization.py -h | --help
-
-Options:
-    -h --help             Show this message.
-    --output_folder=OUT   Output folder for the data and reports to be saved.
-    --country=CNT         Arg for filtering by a specific country
-    --top=top             Top number of countries in the log plot
-"""
-from __future__ import print_function
-import os
-import glob
-import docopt
-import pickle
+import pandas as data
 import os.path
-import pyarrow
-import numpy as np
-import pandas as pd
+import json
+import datetime as dt
 from datetime import datetime
-import matplotlib.pyplot as plt
-from covidify.utils.utils import replace_arg_score
+from regression import *
 
-# plt settings
-font = {'weight' : 'bold',
-        'size'   : 22}
-plt.rc('font', **font)
-plt.style.use('ggplot')
- 
-args = docopt.docopt(__doc__)
-out = args['--output_folder']
-country = args['--country']
-top = int(args['--top'])
+data.options.mode.chained_assignment = None
+samples = 5			#Points to be sampled for regression.
+time_series = data.read_csv("https://raw.githubusercontent.com/coder-amey/COVID-19-Global_Data/master/time-series/Global_aggregated.csv")		#Load the time-series.
 
+latest_tally = time_series.loc[time_series["Date"] == time_series.Date.unique()[-1]]		#Date of last update.
+yest_tally = time_series.loc[time_series["Date"] == time_series.Date.unique()[-2]]		#Previous day's tally.
+predictables = time_series.loc[time_series["Date"] == time_series.Date.unique()[-samples]]	#Dataframe of regions with enough data for a prediction.
 
-if '_' in country:
-    country = replace_arg_score(country)
+#Add new columns to track increments in each type of cases (initialized as current number of cases).
+latest_tally["CNF_inc"] = latest_tally["Confirmed"].copy()
+latest_tally["RCV_inc"] = latest_tally["Recovered"].copy()
+latest_tally["DCS_inc"] = latest_tally["Deceased"].copy()
 
-if country == 'Global':
-    country = None
+#Subtract yesterday's tally from the respective cases from every region.
+for region in yest_tally.Region.unique():
+	window = latest_tally.Region == region
+	window_yest = yest_tally.Region == region
+	idx = latest_tally[window].index
+	latest_tally.loc[idx, "CNF_inc"] = latest_tally[window].iat[0, 2] - yest_tally[window_yest].iat[0, 2]
+	latest_tally.loc[idx, "RCV_inc"] = latest_tally[window].iat[0, 3] - yest_tally[window_yest].iat[0, 3]
+	latest_tally.loc[idx, "DCS_inc"] = latest_tally[window].iat[0, 4] - yest_tally[window_yest].iat[0, 4]
 
-#change report name if country specified
-def create_report_name(country):
-    if country:
-        return '{}_report_{}.xlsx'.format(country, datetime.date(datetime.now()))
-    else:
-        return 'report_{}.xlsx'.format(datetime.date(datetime.now()))
+#Insert and populate columns for active cases and their increments in every region.
+latest_tally["Active"] = latest_tally["Confirmed"] - latest_tally["Recovered"] - latest_tally["Deceased"]
+latest_tally["ACT_inc"] = latest_tally["CNF_inc"] - latest_tally["RCV_inc"] - latest_tally["DCS_inc"]
 
-# Dynamic parameters
-data_dir  = os.path.join(out, 'data', str(datetime.date(datetime.now())))
-agg_file  = 'agg_data_{}.csv'.format(datetime.date(datetime.now()))
-trend_file  = 'trend_{}.csv'.format(datetime.date(datetime.now()))
-log_file  = 'log_{}.csv'.format(datetime.date(datetime.now()))
-report  = create_report_name(country)
+#Calculate and store the predictions for each region. [Vectorized approach]
+cnf_predictables = []
+cnf_matrix =[]
+dcs_predictables = []
+dcs_matrix = []
+latest_tally = latest_tally.set_index("Region")		#Set Region as the index for aligning prediction results.
+for region in predictables.Region.unique():
+	slice = time_series.Region == region
+	if(time_series[slice].Confirmed.tolist()[-samples] > 0):		#Ensure that only non-zero entries are passed.
+		cnf_predictables.append(region)								#Mark as predictable region.
+		cnf_matrix.append(time_series[slice].Confirmed.tolist()[-samples:])
+	
+	if(time_series[slice].Deceased.tolist()[-samples] > 0):		#Ensure that only non-zero entries are passed.
+		dcs_predictables.append(region)								#Mark as predictable region.
+		dcs_matrix.append(time_series[slice].Deceased.tolist()[-samples:])
 
+#Predict new infections:
+predictions = maths.rint(exp_predict(samples, *exp_reg(cnf_matrix)))
+predictions = data.Series(data = predictions, index = cnf_predictables)
+#For regions with insufficient data-points, mark current tally as the next prediction.
+for region in [region for region in latest_tally.index if region not in cnf_predictables]:
+	predictions[region] = latest_tally.loc[region].Confirmed
+latest_tally["CNF_pred"] = predictions
 
-# import data
-print('Importing Data...')
-agg_df = pd.read_csv(os.path.join(data_dir, agg_file))
-daily_df = pd.read_csv(os.path.join(data_dir, trend_file))
-log_df = pd.read_csv(os.path.join(data_dir, log_file))
+#Predict new deaths:
+predictions = maths.rint(exp_predict(samples, *exp_reg(dcs_matrix)))
+predictions = data.Series(data = predictions, index = dcs_predictables)
+#For regions with insufficient data-points, mark current tally as the next prediction.
+for region in [region for region in latest_tally.index if region not in dcs_predictables]:
+	predictions[region] = latest_tally.loc[region].Deceased
+latest_tally["DCS_pred"] = predictions
 
-#Create place to save diagrams
-image_dir =  os.path.join(out,'reports', 'images')
-reports_dir =  os.path.join(out,'reports')
+#Re-index, re-order and sort the columns. Resetting the index is necessary for maintaining compatibility with JSON format.
+latest_tally = latest_tally.reset_index()
+latest_tally = latest_tally[["Region", "Confirmed", "CNF_inc", "Active", "ACT_inc", "Recovered", "RCV_inc", "Deceased", "DCS_inc", "CNF_pred", "DCS_pred"]].sort_values(by = ["Active", "Deceased", "Confirmed"], ascending = False, ignore_index = True)
+latest_tally = latest_tally.set_index("Region")
 
-if not os.path.exists(image_dir):
-    print('Creating reports folder...')
-    os.system('mkdir -p ' + image_dir)
+#Write the table in the JSON file.
+base_dir = os.path.join(os.path.dirname(__file__), "../")		#Obtain the path to the base directory for absolute addressing.
+summary = json.loads(latest_tally.to_json(orient = 'split', index = True))
+summary["update_time"] = datetime.now().strftime("%d %B %Y, %H:%M") + " IST"			#Include last update time.
+with open(base_dir + 'data/Global_summary.json', 'w') as jfile:
+	json.dump(summary, jfile)
 
-# Convert types
-for col in ['confirmed', 'deaths', 'recovered']:
-    agg_df[col] = agg_df[col].replace('', 0).astype(int)
+#Prepare charting data.
+chart_data = dict()
 
+time_series.Date = data.to_datetime(time_series.Date, dayfirst = True)	#Make dates compatible with javascript.
+last_day = time_series.Date.tolist()[-1] + dt.timedelta(days = 1) 			#Last day (including the prediction).
+epoch = time_series.Date.tolist()[0]														#First day of recorded data.
+time_series.Date = time_series.Date.dt.strftime("%Y-%m-%d")				#Shorten dates to save space.
 
-##### Define Graphs #####
-#Change titles and saved file names if country
-#is specified
-def create_title(fig_title, country):
-    if country:
-        return fig_title + ' for ' + country
-    else:
-        return fig_title
-    
-def create_save_file(col, country, graph_type):
-    if country:
-        return '{}_{}_{}.png'.format(country, col, graph_type)
-    else:
-        return '{}_{}.png'.format(col, graph_type)
+chart_data["regions"] = list(latest_tally.index.unique())							#Regions sorted in decreasing order of cases.
+chart_data["interval"] = [epoch.strftime("%Y-%m-%d"), last_day.strftime("%Y-%m-%d")]			#Store range of dates.
+chart_data["update_time"] = datetime.now().strftime("%d %B %Y, %H:%M") + " IST"		#Include last update time.
+chart_data["series"] = dict()
 
-# Plot and save trendline graph
-def create_trend_line(tmp_df, date_col, col, col2, col3, fig_title, country):
-    fig, ax = plt.subplots(figsize=(20,10))
-    tmp_df.groupby([date_col])[[col, col2, col3]].sum().plot(ax=ax, marker='o')
-    ax.set_title(create_title(fig_title, country))
-    fig = ax.get_figure()
-    fig.savefig(os.path.join(image_dir, create_save_file(col, country, 'trendline')))
+#Create and enlist region-wise objects containing series of different cases over time.
+for region in chart_data["regions"]:
+	df = time_series[time_series.Region == region]
+	chart_data["series"][region] = dict()
+	chart_data["series"][region]["Date"] = df.Date.tolist()
+	chart_data["series"][region]["Infected"] = df.Confirmed.astype(int, copy = True).tolist()
+	chart_data["series"][region]["Active"] = (df.Confirmed - df["Recovered"] - df.Deceased).astype(int, copy = True).tolist()
+	chart_data["series"][region]["Recovered"] = df["Recovered"].astype(int, copy = True).tolist()
+	chart_data["series"][region]["Deceased"] = df.Deceased.astype(int, copy = True).tolist()
+	#Read and store predictions for the current region.
+	df = latest_tally[latest_tally.index == region]	
+	chart_data["series"][region]["Prediction"] = df.CNF_pred.tolist() + df.DCS_pred.tolist()
 
-def create_bar(tmp_df, col, rgb, country):
-    tmp_df = tmp_df.tail(120)
-    fig, ax = plt.subplots(figsize=(20,10))
-    tmp = tmp_df.groupby(['date'])[[col]].sum()
-    ax.set_title(create_title(col, country))
-    tmp.plot.bar(ax=ax, rot=90, color=rgb)
-    fig = ax.get_figure()
-    fig.savefig(os.path.join(image_dir, create_save_file(col, country, 'bar')))
-
-def create_stacked_bar(tmp_df, col1, col2, fig_title, country):
-    tmp_df = tmp_df.tail(120)
-    tmp_df = tmp_df.set_index('date')
-    fig, ax = plt.subplots(figsize=(20,10))
-    ax.set_title(create_title(fig_title, country))
-    tmp_df[[col2, col1]].plot.bar(ax=ax,
-                                  rot=90,
-                                  stacked=True)
-    fig = ax.get_figure()
-    fig.savefig(os.path.join(image_dir, create_save_file(col2, country, 'stacked_bar')))
-
-def log_plot(tmp, col, fig_title):
-    '''
-    Plot on a logarithmic scale for comparing
-    countries infection rates
-    
-    '''
-    cm = plt.get_cmap('gist_rainbow')
-    fig = plt.figure(figsize = (20,10))
-    ax = fig.add_subplot(111)
-    ax.set_prop_cycle('color', [cm(1.*i/top) for i in range(top)])
-    for country in tmp.country.unique():
-        new_col = country.lower() + '_' + col
-        tmp[country.lower() + '_' + col] = tmp[[col]]
-        tmp.rename(columns={col: new_col})
-        tmp[tmp.country == country].groupby(['day'])[[new_col]].sum().plot(ax=ax)
-        ax.set_yscale('log', basey=10)
-    ax.set_title(fig_title)
-    fig = ax.get_figure()
-    fig.savefig(os.path.join(image_dir, create_save_file(col, country=None, graph_type='log')))
-    
-##### Create Graphs #####
-print('Creating graphs...')
-print('... Time Series Trend Line')
-# Time Series Data Plots
-create_trend_line(agg_df, 'file_date', 'confirmed', 'deaths', 'recovered', 'Accumulative trend', country)
-
-
-print('... Daily Figures')
-# Daily Figures Data Plots
-daily_figures_cols = ['new_confirmed_cases', 'new_deaths', 'new_recoveries', 'currently_infected']
-for col, rgb in zip(daily_figures_cols, ['tomato', 'lightblue', 'mediumpurple', 'green']):
-    create_bar(daily_df, col, rgb, country)
-
-# Trend line for new cases
-create_trend_line(daily_df, 'date', 'new_confirmed_cases', 'new_deaths', 'new_recoveries', 'Daily trendline', country)
-
-
-print('... Daily New Infections Differences')
-new_df = pd.DataFrame([])
-new_df['date'] = daily_df['date']
-new_df['confirmed_cases'] = agg_df.groupby(['file_date']).confirmed.sum().values - daily_df.new_confirmed_cases
-new_df['new_confirmed_cases'] = daily_df.new_confirmed_cases
-create_stacked_bar(new_df, 'new_confirmed_cases', 'confirmed_cases', "Stacked bar of confirmed and new cases by day", country)
-
-
-print('... Logarithmic plots')
-log_plot(log_df, 'confirmed', 'Logarithmic plots for top {} most infected countries\nStarting from days since first 500 confirmed cases.'.format(top))
-
-### Create Excel Spreadsheet ###
-print('Creating excel spreadsheet report...')
-workbook_writer = pd.ExcelWriter(os.path.join(reports_dir, report), engine='xlsxwriter')
-
-# Add daily summary to spreadsheet
-daily_df.to_excel(workbook_writer, sheet_name='daily figures')  
-workbook = workbook_writer.book
-
-def get_image_types(path):
-    '''
-    get all the possible types of images in
-    the passed directory path
-    '''
-    types = []
-    for fn in glob.glob(os.path.join(path, '*.png')):
-        types.append(fn.split('_',)[-1].split('.')[0])
-    
-    return types
-
-# Get all images for each type
-def read_images(path, graph_type):
-    image_list = []
-    for fn in glob.glob(os.path.join(path, '*_{}.png'.format(graph_type))):
-        image_list.append(fn)    
-    images = {graph_type : image_list}
-    return dict(images)
-
-image_types = get_image_types(image_dir)
-
-padding = 1 # Set padding for images in spreadsheet
-for types in set(image_types):
-    print('... reading images for:', types)
-    type_dict = read_images(image_dir, types)
-    
-    # Add image to the worksheet
-    worksheet = workbook.add_worksheet(name='{}_graphs'.format(types))
-    for image in type_dict[types]:
-        worksheet.insert_image('A' +str(padding), image) 
-        padding += 50
-    padding = 1
-    
-workbook.close()
-print('Done!')
+#Write the charting data in the JSON file.
+with open(base_dir + 'data/Global_series.json', 'w') as jfile:
+	json.dump(chart_data, jfile)
